d8dab794f9ed2d1347a6851f7dd985136ac15af8
alis_git <ra.perera15@gmail.com>
2026-01-08 11:41:51 +1100

    final working app

--- apirequest.py
+++ apirequest.py
@@ -11,33 +11,38 @@ def get_data(place, days, kind="Temperature"):
     geocoding_url = f"http://api.openweathermap.org/geo/1.0/direct?q={place}&limit=5&appid={API_KEY}"
     # getting lat lon coordinates by place name
     geocoding_response = requests.get(geocoding_url, timeout=5)
-    print(geocoding_response.status_code)
     print(geocoding_response.url)
+    print(geocoding_response.status_code)
     geocoding_data = geocoding_response.json()
-    lat = geocoding_data[0]["lat"]
-    lon = geocoding_data[0]["lon"]
-    print("Received results for coordinates: ", geocoding_data[0]["lat"], geocoding_data[0]["lon"])
-    
+    print(geocoding_data)
 
-    weather_url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=metric&appid={API_KEY}"
-    weather_request = requests.get(weather_url, timeout=5)
-    weather_data = weather_request.json()
-    # print("Weather response: ", weather_data['list'][0])
-    list_data = weather_data['list']
-    # Selecting only data for the given number of days: 
-    forecast_data = list_data[:days*8]  # 8 data points per day (3-hour intervals)
+    if len(geocoding_data) <1:
+        return None, None
+    else:
+        lat = geocoding_data[0]["lat"]
+        lon = geocoding_data[0]["lon"]
+        print("Received results for coordinates: ", geocoding_data[0]["lat"], geocoding_data[0]["lon"])
+        
 
-    if kind == "Temperature":
-        dispaly_temp  = [temp['main']['temp'] for temp in forecast_data] # contains temp per hour for given number of days
-        print("Temperature data: ", dispaly_temp)
-        display_dates = [dt['dt_txt'] for dt in forecast_data]
-        print("Dates to display  ", display_dates)
-        return display_dates, dispaly_temp
-    elif kind == "Sky":
-        sky_conditions = [sky['weather'][0]['description'] for sky in forecast_data]
-        print("Sky conditions: ", sky_conditions)
-        display_dates = [dt['dt_txt'] for dt in forecast_data]
-        return display_dates, sky_conditions
+        weather_url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=metric&appid={API_KEY}"
+        weather_request = requests.get(weather_url, timeout=5)
+        weather_data = weather_request.json()
+        # print("Weather response: ", weather_data['list'][0])
+        list_data = weather_data['list']
+        # Selecting only data for the given number of days: 
+        forecast_data = list_data[:days*8]  # 8 data points per day (3-hour intervals)
+
+        if kind == "Temperature":
+            dispaly_temp  = [temp['main']['temp'] for temp in forecast_data] # contains temp per hour for given number of days
+            #print("Temperature data: ", dispaly_temp)
+            display_dates = [dt['dt_txt'] for dt in forecast_data]
+            #print("Dates to display  ", display_dates)
+            return display_dates, dispaly_temp
+        elif kind == "Sky":
+            sky_conditions = [sky['weather'][0]['description'] for sky in forecast_data]
+            print("Sky conditions: ", sky_conditions)
+            display_dates = [dt['dt_txt'] for dt in forecast_data]
+            return display_dates, sky_conditions
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -13,10 +13,27 @@ st.subheader(f"{option} for the next {days} days in the {place}")
 # Creating a chart now
 if place:
     d, t = get_data(place, days, option)
-
-    if option == "Temperature":
-        print("Plotting Temperature Data")
-        figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (°C)"}, title="Temperature Trend")
-        st.plotly_chart(figure)
-    elif option == "Sky":
-        st.image()
+    if d is None and t is None:
+        st.error("Location not found. Please enter a valid city or country name.")
+    else:
+        if option == "Temperature":
+            print("Plotting Temperature Data")
+            figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (°C)"},
+                             title="Temperature Trend")
+            st.plotly_chart(figure)
+        elif option == "Sky":
+            # associations of images and sky conditions:
+            image_base = {
+                "clear sky": "images/clear.png",
+                "few clouds": "images/cloud.png",
+                "overcast clouds": "images/cloud.png",
+                "scattered clouds": "images/cloud.png",
+                "broken clouds": "images/cloud.png",
+                "shower rain": "images/rain.png",
+                "light rain": "images/rain.png",
+                "rain": "images/rain.png",
+                "light snow": "images/snow.png",
+                "snow": "images/snow.png",
+            }
+            images_display = [image_base[condition] for condition in t]
+            st.image(images_display, caption=t, width=110,)
